77f2d9f80763ef6fb88a27dbe30247f94b300866
Stan <stan.mikhnevich@gmail.com>
2025-03-13 01:32:53 +0200

    Updated api route

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\v1\ApiPositionController;
 use App\Http\Controllers\Api\v1\ApiTokenController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('token', ApiTokenController::class);
+
+Route::apiResource(
+    'positions',
+    ApiPositionController::class
+)->only('index', 'show');
+
