f3d2a85082d224214db4223e54f46663f7dc227f
Stan <stan.mikhnevich@gmail.com>
2025-03-13 01:50:17 +0200

    Added user observer

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\RegisterToken;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserObserver
+{
+    public ?string $token;
+
+    public function __construct(Request $request)
+    {
+        $this->token ??= $request->header('Token');
+    }
+
+    public function created(User $user): void
+    {
+        RegisterToken::where('token', $this->token)->valid()->delete();
+    }
+
+}
